4e033cd82afcbe66107b13352317f7725b6fcda6
Cesar Menegatti <cesinha51@gmail.com>
2021-04-10 00:26:23 -0300

    middleware removed from Category

--- src/modules/categories/infra/http/routes/categories.routes.ts
+++ src/modules/categories/infra/http/routes/categories.routes.ts
@@ -3,13 +3,9 @@ import { celebrate, Joi, Segments } from 'celebrate';
 
 import CategoriesController from '../controllers/CategoriesController';
 
-import ensureAuthenticated from '@modules/users/infra/middlewares/ensureAuthenticated';
-
 const categoryRoutes = Router();
 
 const categoriesController = new CategoriesController();
-
-categoryRoutes.use(ensureAuthenticated);
 
 categoryRoutes.post(
   '/',
